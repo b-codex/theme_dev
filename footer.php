1fce1b0c4659d5361576122e1e9d5085d3049072
sintayehu-B <it.sintayehu.sermessa@gmail.com>
2022-01-13 08:24:43 +0300

    fixed the footer.php file to the formatted way

--- footer.php
+++ footer.php
@@ -1,7 +1,18 @@
-<h3>Developed by NoobDevTeam @2021</h3>
-<?php wp_footer();?>
-
-
-<?php
-            wp_nav_menu( array("theme_location" => 'footer-menu') )
-        ?>
+<h3>Developed by  @2021</h3>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name')?></title>
+</head>
+<body <?php body_class()?>>
+    <hr>
+    <footer class="site-footer">
+        <p><?php bloginfo('name')?></p>
+    </footer>
+    <p> Developed by NoobDevTeam &copy; 2021</p>
+    <?php wp_footer() ?>
+</body>
+</html>
