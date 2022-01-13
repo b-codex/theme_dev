6e5bfc6ee12472c6d618893ccbec214d0e6d1d01
sintayehu-B <it.sintayehu.sermessa@gmail.com>
2022-01-13 08:27:05 +0300

    added menu theme support in function.phph file

--- functions.php
+++ functions.php
@@ -1,5 +1,23 @@
 <?php
+// add menu support
+add_theme_support('menus');
 
+
+//Register nav menu
+
+function rt_add_menu(){
+    register_nav_menus(
+        array(
+            'top-menu' => __('Top Menu'),
+            'footer-menu' => __('Footer Menu'),
+            'secondary-menu' => __('Secondary Menu'),
+        )
+        );}
+
+
+// add the hook to make the menu
+
+add_action('init', 'rt_add_menu');
     function td_register_styles(){
         $version = wp_get_theme()->get( 'Version' );
 
